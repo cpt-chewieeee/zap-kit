a30962cc8faf5ec4f803af4cb123a0f6729e5265
hongyi zhang <hzhan016@gmail.com>
2018-10-06 18:03:30 -0700

    adding in example route

---
+++ src/Ruler/tools.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class Tools extends React.Component {
+  render () {
+
+    
+
+    return (
+
+        <div className='tool-controller'>
+          <div className='square'>
+            {/* <img src={ToolIcon} /> */}
+          </div>
+        </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 
+
 export Ruler from './Ruler'

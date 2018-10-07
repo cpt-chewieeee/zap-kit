560c74eb66330e02aeeb37a2c05ca43a033a4a34
hongyi zhang <hzhan016@gmail.com>
2018-10-06 18:09:46 -0700

    set up another example

--- example/src/routes/RulerWithView/index.js
+++ example/src/routes/RulerWithView/index.js
@@ -1,9 +1,35 @@
 import React from 'react'
-
+import { Ruler } from 'zap-kit'
+import './style.css'
 export default class RulerWithView extends React.Component {
-  render () {
+  constructor(props) {
+    super(props)
+    this.refContainer = React.createRef()
+    this.state = {
+      container: null
+    }
+  }
+  componentDidMount() {
+    this.setState({
+      container: this.refContainer
+    })
+  }
+  render() {
     return (
-      <div>hllo rom here'</div>
+      <div style={{
+        height: '1050px',
+        width: '4050px'
+      }}
+        ref={ref => this.refContainer = ref}
+      >
+        <Ruler
+          refContainer={this.state.container}>
+
+          <div className='display-panel' style={{ backgroundColor: 'red', height: '100%' }}>
+            Display Content
+          </div>
+        </Ruler>
+      </div>
     )
   }
 }
